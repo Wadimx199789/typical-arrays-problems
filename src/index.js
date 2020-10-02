47cf77543bb171a0495d588fc99c1a7a54aed065
Wadimx199789 <martinkewichwadim@gmail.com>
2020-10-02 12:32:58 +0300

    feat: decidet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array||array.length == 0) return 0;
+  else{
+  let temp=array[0];
+  for(let el of array){
+    if(el<temp) temp=el;
+  }
+  return temp;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array||array.length == 0) return 0;
+  else{
+  let temp=array[0];
+  for(let el of array){
+    if(el>temp) temp=el;
+  }
+  return temp;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array||array.length == 0) return 0;
+  else{
+  let sum=0;
+  for(let el of array){
+    sum+=el
+  }
+  return sum/array.length;
+  }
 }
